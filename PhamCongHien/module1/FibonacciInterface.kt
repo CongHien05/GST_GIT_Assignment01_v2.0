28811fe7e472047586c5c2670306319f2b6622a8
CongHien05 <phamconghien18@gmail.com>
2026-06-19 09:47:22 +0700

    Implement Fibonacci vòng lặp for trên nhánh impl2

--- PhamCongHien/module1/FibonacciInterface.kt
+++ PhamCongHien/module1/FibonacciInterface.kt
@@ -1,3 +1,15 @@
-interface FibonacciInterface {
-    fun Fibonacci(n: Int, flag: Boolean)
+class FibonacciImpl2 : FibonacciInterface {
+    override fun Fibonacci(n: Int, flag: Boolean): Int {
+        if (flag) return -1 // Chỉ chạy for khi flag = False
+        if (n <= 1) return n
+        var a = 0
+        var b = 1
+        var sum = 0
+        for (i in 2..n) {
+            sum = a + b
+            a = b
+            b = sum
+        }
+        return sum
+    }
 }
